3bd22a0a1cb184b31ad41283d638677478b63976
Gabrielle Littlefair <g.littlefair@live.co.uk>
2024-03-17 21:36:45 +0000

    PlaneAutomaticResponse comments

--- Aircraft_Marshalling_Training_v01/Assets/Scripts/PlaneAutomaticResponse.cs
+++ Aircraft_Marshalling_Training_v01/Assets/Scripts/PlaneAutomaticResponse.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class PlaneAutomaticResponse : MonoBehaviour
 {
+
+    public bool isRunning; // This determines whether we have started running the simulation 
+    public bool sticksInHands; // This determines whether the Marshal is holding sticks
+
     private float horizontalInput, verticalInput;
     private float currentSteerAngle, currentbreakForce;
     private float threshold;
@@ -39,6 +43,7 @@ public class PlaneAutomaticResponse : MonoBehaviour
 
     private void Start()
     {
+
         LeftStick = GameObject.FindGameObjectWithTag("LeftStick");
         RightStick = GameObject.FindGameObjectWithTag("RightStick");
 
@@ -54,41 +59,49 @@ public class PlaneAutomaticResponse : MonoBehaviour
 
         LeftStickisMoving = false;
         RightStickisMoving = false;
-        isBreaking = true;
+        isBreaking = true; // this might be unnecessary, we can probably remove it and use isMoving for both 
         isMoving = false;
+        isRunning = true; // TODO: this needs to be updated and linked with the button!
+        sticksInHands = true; // TODO: this needs to be updated - from the controller information I believe (have they grabbed and not dropped etc) 
     }
 
     private void RecordLeftPosition(GameObject stick)
     {
+        // Keep track of recent LeftStick position and orientation
+        // so that we can search for start positions of movements 
         leftPositionHistory.Add(stick.transform.position);
         leftOrientationHistory.Add(stick.transform.localEulerAngles);
     }
 
     private void RecordRightPosition(GameObject stick)
     {
+        // Keep track of recent RightStick position and orientation
+        // so that we can search for start positions of movements 
         rightPositionHistory.Add(stick.transform.position);
         rightOrientationHistory.Add(stick.transform.eulerAngles);
     }
 
     private void PlaneMoving()
     {
+        // This function checks if the sticks (both sticks) are currently in the end position of the GO movement.
+        // It then searches through recent history, to see if both have been in the start position of the GO movement.
+        // If they have, then it turns on the plane forward motion.
+
         threshold = 50;
-        // This compares the current orientation with the end orientation (pointing straight down) 
+        // This compares the current orientation with the end orientation (sticks points down) 
         float left_angle_go = Vector3.Angle(LeftStick.transform.localEulerAngles, new Vector3(90, 0, 45));
         float right_angle_go = Vector3.Angle(RightStick.transform.localEulerAngles, new Vector3(90, 180, 180));
 
-        // Checks if the current position is at the correct height, and is within a threshold of the desired orientation
+        // Left Stick: Check if the current position is at the correct height, and is within a threshold of the desired orientation
         if ((LeftStick.transform.position.y < 1.3) && (Mathf.Abs(left_angle_go) < threshold))
         {
-            // Now we look through the recent history for the start position of the movement 
+            // Now search for the start GO position 
             Vector3 orient;
             for (int i = 0; i < maxHistorySize; i++)
             {
                 orient = leftOrientationHistory[maxHistorySize - i - 1];
                 left_angle_go = Vector3.Angle(orient, new Vector3(270, 0, 0));
 
-                // With an extra condition, that the start position must occur at a point higher than the previous step
-                // This is to fix the issue of the plane going immediately after it stops.
                 if ((leftPositionHistory[maxHistorySize - i - 1].y > 1.7)&&(Mathf.Abs(left_angle_go) < threshold))
                 {
                     LeftStickisMoving = true;
@@ -97,16 +110,16 @@ public class PlaneAutomaticResponse : MonoBehaviour
             }
         }
 
-        // We only check the RightStick position if the LeftStick is in the correct position
+        // Right Stick: We only check the RightStick position if the LeftStick is in the correct position
+        // Now check for the end position of the GO movement 
         if ((LeftStickisMoving) && (RightStick.transform.position.y < 1.3) && (Mathf.Abs(right_angle_go) < threshold))
         {
-            
+            // Check for the start position of the GO movement 
             Vector3 orient;
             for (int i = 1; i < maxHistorySize; i++)
             {
                 orient = rightOrientationHistory[i];
                 right_angle_go = Vector3.Angle(orient, new Vector3(270, 0, 0));
-                // Same conditions as for the left stick 
                 if ((rightPositionHistory[i].y > 1.7) && (Mathf.Abs(right_angle_go) < threshold))
                 {
                     RightStickisMoving = true;
@@ -115,7 +128,7 @@ public class PlaneAutomaticResponse : MonoBehaviour
                     break;
                 }
             }
-            // Here, we say that if only the left stick is doing the motion, then both need to be set to false. 
+            // Update: if the Left Stick passed the test but the right stick did not, then we need to set both as false. 
             if (!RightStickisMoving)
             {
                 LeftStickisMoving = false;
@@ -128,20 +141,21 @@ public class PlaneAutomaticResponse : MonoBehaviour
 
     private void PlaneStopping()
     {
+        // This function checks if the sticks (both sticks) are currently in the end position of the STOP movement.
+        // It then searches through recent history, to see if both have been in the start position of the STOP movement.
+        // If they have, then it turns on the breaks and stops the plane.
+
         threshold = 40;
-        // This compares the current orientation with the end orientation (sticks crossed above head) 
+        // This compares the current orientation and position with the desired end (sticks crossed above head) 
         float left_angle_stop = Vector3.Angle(LeftStick.transform.localEulerAngles, new Vector3(300, 90, 180));
         float right_angle_stop = Vector3.Angle(RightStick.transform.localEulerAngles, new Vector3(300, 180, 180));
-
-        // Check for stop if the plane is moving and the end of the stop movement occurs 
         if ((LeftStick.transform.position.y > 1.7) && (Mathf.Abs(left_angle_stop) < threshold))
         {
-            // Now check for the starting position of the forward motion (arms out to the sides) 
+            // Now check for the starting position of the STOP motion (arms out to the sides) 
             Vector3 orient;
             for (int i = 0; i < maxHistorySize; i++)
             {
                 orient = leftOrientationHistory[maxHistorySize - i - 1];
-                // TODO CHECK THIS ANGLE
                 left_angle_stop = Vector3.Angle(orient, new Vector3(0, 250, 0));
                 if ((leftPositionHistory[i].y < 1.5) && (Mathf.Abs(left_angle_stop) < threshold))
                 {
@@ -151,23 +165,22 @@ public class PlaneAutomaticResponse : MonoBehaviour
             }
         }
 
-        // Only check the RightStick if the LeftStick is in the correct position 
+        // Only check the Right Stick if the Left Stick is in the correct position 
         if ((!LeftStickisMoving) && (RightStick.transform.position.y > 1.7) && (Mathf.Abs(right_angle_stop) < threshold))
         {
-            // Now check for the starting position of the forward motion
+            // Now check for the starting position of the STOP motion
             Vector3 orient;
             for (int i = 0; i < maxHistorySize; i++)
             {
                 orient = rightOrientationHistory[i];
                 right_angle_stop = Vector3.Angle(orient, new Vector3(0, 70, 0));
-
                 if ((rightPositionHistory[i].y < 1.5) && (Mathf.Abs(right_angle_stop) < threshold))
                 {
                     RightStickisMoving = false;
                     break;
                 }
             }
-            // Here, we say that if only the left stick is doing the stop motion, then the plane does not stop. 
+            // Update: if the Left Stick passed the test but the right stick did not, then we need to set both as true.
             if (RightStickisMoving)
             {
                 LeftStickisMoving = true;
@@ -185,6 +198,9 @@ public class PlaneAutomaticResponse : MonoBehaviour
 
     private void PlaneTurningLeft()
     {
+        // This function checks that a certain position occurs for the turning to be switched on
+        // (left arm bent at the elbow, right arm pointing to the side)
+
         float threshold1 = 60;
         float threshold2 = 50;
 
@@ -202,6 +218,8 @@ public class PlaneAutomaticResponse : MonoBehaviour
 
     private void PlaneTurningRight()
     {
+        // This function checks that a certain position occurs for the turning to be switched on
+        // (right arm bent at the elbow, left arm pointing to the side)
         float threshold1 = 60;
         float threshold2 = 50;
 
@@ -219,6 +237,10 @@ public class PlaneAutomaticResponse : MonoBehaviour
 
     private void PlaneStopTurningLeft()
     {
+        // This function checks that certain criteria are still true while the plane is turning left
+        // 1. LeftStick has to be at most 0.1m below the RightStick - to allow for the turning movement
+        // 2. LeftStick drops below a certain height (1.4m)
+
         if ((LeftStick.transform.localPosition.y - RightStick.transform.localPosition.y <= -0.1) || (LeftStick.transform.localPosition.y < 1.4))
         {
             isTurningLeft = false;
@@ -227,6 +249,9 @@ public class PlaneAutomaticResponse : MonoBehaviour
 
     private void PlaneStopTurningRight()
     {
+        // This function checks that certain criteria are still true while the plane is turning right
+        // 1. RightStick has to be at most 0.1m below the LeftStick - to allow for the turning movement
+        // 2. RightStick drops below a certain height (1.4m)
         if ((RightStick.transform.localPosition.y - LeftStick.transform.localPosition.y <= -0.1) || (RightStick.transform.localPosition.y < 1.4))
         {
             isTurningRight = false;
@@ -250,73 +275,52 @@ public class PlaneAutomaticResponse : MonoBehaviour
         }
 
         // Only want to do one thing per time step!
-        if (!isMoving)
-        {
-            PlaneMoving();
-        }
-        else if (isMoving && (!isTurningLeft) && (!isTurningRight))
-        {
-            PlaneStopping();
-            if (!isBreaking)
+        // Hence ELSE.
+
+        if (isRunning && sticksInHands) {
+            // Priority if stopped is GO
+            if (!isMoving)
             {
-                PlaneTurningRight();
-                if (!isTurningRight)
+                PlaneMoving();
+            }
+            // Priority if moving is STOP
+            else if (isMoving && (!isTurningLeft) && (!isTurningRight))
+            {
+                PlaneStopping();
+
+                // if not STOP but moving, check for turns
+                if (!isBreaking)
                 {
-                    PlaneTurningLeft();
+                    PlaneTurningRight();
+                    // if it is not turning right, then check for turning left
+                    if (!isTurningRight)
+                    {
+                        PlaneTurningLeft();
+                    }
                 }
             }
+            // Priority if is is moving and turning is stop turning
+            else if (isMoving && isTurningRight)
+            {
+                PlaneStopTurningRight();
+            }
+            else if (isMoving && isTurningLeft)
+            {
+                PlaneStopTurningLeft();
+            }
         }
-        else if (isMoving && isTurningRight)
-        {
-            PlaneStopTurningRight();
-        }
-        else if (isMoving && isTurningLeft)
-        {
-            PlaneStopTurningLeft();
-        }
+        
 
         GetInput();
         HandleMotor();
         HandleSteering();
         UpdateWheels();
-
-        //if (isMoving && isBreaking)
-        //{
-        //    Debug.Log("isMoving AND isBreaking !!!!");
-        //}
-        //if ((!isMoving) && (!isBreaking))
-        //{
-        //    Debug.Log(" NEITHER isMoving OR isBreaking !!!!");
-        //}
-
-        //if ((isTurningLeft) && (isTurningRight))
-        //{
-        //    Debug.Log(" BOTH isTurningLeft AND isTurningRight !!!!");
-        //}
-
-        //if (isMoving)
-        //{
-        //    Debug.Log("isMoving");
-        //}
-        //if (isTurningLeft)
-        //{
-        //    Debug.Log("Left Turn");
-        //}
-        //if (isTurningRight)
-        //{
-        //    Debug.Log("Right Turn");
-        //}
-        //if (isBreaking)
-        //{
-        //    Debug.Log("Break!");
-        //}
     }
 
     private void GetInput()
     {
         
         // input from steering
-
         if (isTurningLeft)
         {
             horizontalInput = -1;
